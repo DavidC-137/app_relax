6583332888523a58411329c70f3237030e098ed5
David Zavala <davidzu.ti20@utsjr.edu.mx>
2023-07-18 21:34:06 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:ambien/screens/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.compact
+      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Ambient Mode',
+      home: const StartScreen(),
+    );
+  }
+}
